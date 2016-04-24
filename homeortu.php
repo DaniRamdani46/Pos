7db95d1d6f33b892184714c21d2e6906bb3f3e71
ciciheryanti <ciciheryanti7@gmail.com>
2016-04-24 16:04:34 +0700

    homeortu.php
    untuk mengenai informasi vaksin

---
+++ homeortu.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+		<title>POSYANDU_SUKALAYA</title>
+		
+	</head>
+<body Background="img/1000.jpg">
+
+<br>
+<table align="center"  width="907"border=1 bordercolor=#84faea >
+<tr>
+
+			<th colspan=5 ><img src="img/PPIEU.jpg" width="907" height="208"></th>
+			</tr>
+			
+			</table>
+			<table align="center"  width="917"border=1 bordercolor=#84faea >
+			<tr>
+			<td width="137">
+			<a href="homeposyandu.php" title="halaman home">
+			<b><center><font size=2 font face="comic sans Ms" font color=#063b48>
+			Home</a> </b></center></td>
+	
+
+			
+			<TD width="146"><a href="datahadir.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Data hadir</center></td>
+			<TD width="170"><a href="databali.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Data Balita dan Imunisasi</b></center></td>
+			</TABLE>
+		
+			
+			
+			<table align="center"  width="920" >
+			
+			<tr><th Background="img/kl.gif" width="920" height="23" ></th></tr>
+			</table>
+			
+			
+			
+		
+			
+			<br>
+<table align="center" width="914"  Background="img/ba.jpg">
+			<tr><td>
+<align="left" ><font size=3 font face="Berlin sanS FB" font color=#063b48><B>Posyandu Bina Sejahtera</B> merupakan posyandu yang mencakup lingkungan di sukalaya barat, pusat pelayanan terpadu ini memiliki anggota anggota 7 orang, sering di sebut dengan kader posyandu, yang merupakan perwakilan dari tiap RT 
+dimana di adakan setiap bulan sekali, yaitu kegiatan imunisasi balita dan pemberian vaksin. dan kegitan penyuluhan lainnya.			</font></tr></td>
+			<tr> <td><hr width=100% ></hr>
+</tr></td>
+
+<tr><td><center>	<img src="img/vak.gif" width=200></center> </tr></td>
+<tr><td>
+		<table border=1 align="center" > 
+						<tr><td>Kode vaksin</td>
+						<td> Nama vaksin</td> 
+						<td> Manfaat </td>
+						</tr>
+						<tr><td>fag</td><td>fsfgag</td><td>faga</td>
+						</tr>
+						</table>
+						</tr></td>
+				 </table>
+</body>
+</html>
