a3a327fb41595691732092a3a0f36bb667e24d13
ciciheryanti <ciciheryanti7@gmail.com>
2016-04-24 16:30:16 +0700

    inputdata.php
    untuk inputan data yang didalamnya ada input data kader input data hadir piket dan input data balita dan imunisasi di posyandu sukalaya

---
+++ inputdata.php
@@ -0,0 +1,88 @@
+<html>
+	<head>
+		<title>input data</title>
+	</head>
+<body Background="img/1000.jpg">
+<br>
+<table align="center"  width="907"border=1 bordercolor=#84faea >
+<tr>
+
+			<th colspan=5 ><img src="img/PPIEU.jpg" width="907" height="208"></th>
+			</tr>
+			
+			</table>
+			<table align="center"  width="917"border=1 bordercolor=#84faea >
+			<tr>
+			<td width="137">
+			<a href="homeposyandu.php" title="halaman home">
+			<b><center><font size=2 font face="comic sans Ms" font color=#063b48>
+			Home</a> </b></center></td>
+	
+			
+			<TD width="146"><a href="inputdata.php" title="Silahkan Klik untuk input data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Input Data </center></td>
+			
+			<TD width="146"><a href="datakader.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Data Kader</center></td>
+			
+			<TD width="146"><a href="datahadir.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Data hadir</center></td>
+			<TD width="170"><a href="databali.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Data Balita dan Imunisasi</b></center></td>
+			<TD width="170"><a href="upload.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Upload</b></center></td>
+			<TD width="170"><a href="galery.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Gallery</b></center></td>
+			<TD width="170"><a href="chat.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Chat</b></center></td>
+			<TD width="170"><a href="index.html" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			LOGOUT</b></center></a></td>
+			</TABLE>
+		
+			
+			
+			<table align="center"  width="920" >
+			
+			<tr><th Background="img/kl.gif" width="920" height="23" ></th></tr>
+			</table>
+			<table align="center"  width="910" >
+			<tr><td><font size=3 font face="Harlow Solid Italic" font color=#063b48><center><marquee>
+			Silahkan Pilih Input Datanya</marquee></center></td></tr>
+			<tr> <td><hr width=100% ></hr></table>
+			
+			
+			<table align="center"  width="910" border=1 bordercolor=#84faea >
+			<tr><TD width="100" HEIGHT="36" BGCOLOR=#063b48><a href="putkader.php" title="Silahkan Klik untuk input data Kader">
+			<center><font size=2 font face="Capture it" font color=#84faea>
+			INPUT DATA KADER</center></td><TD ROWSPAN=3 WIDTH="410">&nbsp &nbsp <font size=5 font face="Freehand521 BT" font color=#063b48>
+			Posyandu SUKALAYA
+			<br> &nbsp
+			<br> &nbsp </td>
+			</TR>
+			<TR><TD width="100" HEIGHT="36" BGCOLOR=#063b48><a href="puthadir.php" title="Silahkan Klik untuk input data hadir piket">
+			<center><font size=2 font face="Capture it" font color=#84faea>
+			INPUT HADIR PIKET</center></td></TR>
+			<TR><TD width="100" HEIGHT="36" BGCOLOR=#063b48><a href="putbalita.php" title="Silahkan Klik untuk input data balita dan imunisasi">
+			<center><font size=2 font face="Capture it" font color=#84faea>
+			Input Data Balita dan imunisasi</center></td></TR>
+			</TABLE>
+			
+			
+			
+		
+			
+			<br>
+			
+						</tr></td>
+				 </table>
+				 </table>
+				</body>
+				</html>
