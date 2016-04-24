1246e28583c43c16254cdc6533c28678c81aed94
ciciheryanti <ciciheryanti7@gmail.com>
2016-04-24 16:12:34 +0700

    homeposyandu.php
    untuk suatu mengenai informasi vaksin dan keterangan manfaat dari vaksin

---
+++ homeposyandu.php
@@ -0,0 +1,117 @@
+<html>
+	<head>
+		<title>POSYANDU SUKALAYA</title>
+<!--Start of Zopim Live Chat Script-->
+<script type="text/javascript">
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="//v2.zopim.com/?3bONl6zM8MntbeF5D3hTtdW7UlJmOwzY";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+</script>
+<!--End of Zopim Live Chat Script-->
+	</head>
+<body Background="img/1000.jpg">
+<br>
+<table align="center"  width="907"border=1 bordercolor=#84faea >
+<tr>
+
+			<th colspan=4 ><img src="img/PPIEU.jpg" width="907" height="208"></th>
+			</tr>
+			
+			</table>
+			<table align="center"  width="917"border=1 bordercolor=#84faea >
+			<tr>
+			<td width="137">
+			<a href="homeposyandu.php" title="halaman home">
+			<b><center><font size=2 font face="comic sans Ms" font color=#063b48>
+			Home</a> </b></center></td>
+	
+			
+			<TD width="146"><a href="inputdata.php" title="Silahkan Klik untuk input data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Input Data </center></td>
+			
+			<TD width="146"><a href="datakader.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Data Kader</center></td>
+			
+			<TD width="146"><a href="datahadir.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Data hadir</center></td>
+			<TD width="170"><a href="databali.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Data Balita dan Imunisasi</b></center></td>
+			<TD width="170"><a href="upload.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Upload</b></center></td>
+			<TD width="170"><a href="galery.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Gallery</b></center></td>
+			<TD width="170"><a href="chat.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Chat</b></center></td>
+			<TD width="170"><a href="index.html" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Logout</b></center></a></td>
+			</TABLE>
+		
+			
+			
+			<table align="center"  width="920"  >
+			
+			<tr><th Background="img/kl.gif" width="920" height="23"  ></th></tr>
+			</table>
+			
+			
+			<table align="center"  width="918" >
+			<tr><td><font size=4 font face="Harlow Solid Italic" font color=#063b48><center><marquee>
+			Pelayanan Sukalaya Barat</marquee></center></td></tr>
+			</table>
+			
+			<table align="center" width="914"  Background="img/ba.jpg" border=1 bordercolor=#84faea>
+			<tr><td>
+<table align="center" width="914"  Background="img/ba.jpg" >
+			<tr><td>
+<align="left" ><font size=4 font face="Freehand521 BT" font color=#063b48>Posyandu Sukalaya</B> <font size=3 font face="Berlin sanS FB" font color=#063b48>merupakan posyandu yang mencakup lingkungan di Sukalaya Barat RT 04 RW 08,pelayanan terpadu ini memiliki anggota anggota 5 orang, sering di sebut dengan kader posyandu, yang merupakan perwakilan dari tiap RT 
+dimana di adakan setiap bulan sekali, yaitu kegiatan imunisasi balita dan pemberian vaksin. dan kegitan penyuluhan lainnya.</font></tr></td>
+			
+</tr></td>
+<tr><td><hr width=100% ></hr></tr></td>
+
+<tr><td><font size=4 font face="Freehand521 BT" font color=#063b48>
+			Informasi Vaksin	 </tr></td>
+<tr><td>
+
+
+
+		<table border=1   bordercolor=#84faea align="center" > 
+						<tr BGCOLOR=#063b48><td width="120"> <font size=2 font face="comic sans Ms" font color=#84faea><b><center>Kode vaksin</b></center></td>
+						<td width="130"> <font size=2 font face="comic sans Ms" font color=#84faea><b><center>Nama vaksin</b></center></td> 
+						<td width="735"> <font size=2 font face="comic sans Ms" font color=#84faea><b><center>Keterangan  dan Manfaat</b></center</td>
+						</tr>
+						<tr><td><font color=#063b48>vk1 </td><td><font color=#063b48>Vaksin BCG</td><td><font color=#063b48>untuk mencegah penyakit TBC. Jika bayi sudah berumur lebih dari tiga bulan, harus dilakukan uji tuberkulin terlebih dulu. BCG dapat diberikan apabila hasil uji tuberkulin negatif</td>
+						</tr>
+						<td><font color=#063b48>vk2 </td><td> <font color=#063b48>vaksin polio</td><td> <font color=#063b48>pemberian pada anak dengan meneteskan pada mulut. Kemasan sebanyak 1 cc / 2 cc dalam 1 ampul. guna untuk meminimalisir penyebaran virus polio</td>
+						</tr>
+						</td>
+						<td><font color=#063b48> vk3</td><td><font color=#063b48> vaksin DPT</td><td> <font color=#063b48>iberikan untuk mencegah tiga macam penyakit sekaligus, yaitu Difteri, Tetanus, dan Pertusis. Vaksin ini diberikan pertama kali saat bayi berumur lebih dari enam minggu. Lalu saat bayi berumur 4 dan 6 </td>
+						</tr>
+						<td> <font color=#063b48>vk4</td><td> <font color=#063b48>vaksin campak</td><td><font color=#063b48>menecgah penyakit campak Campak pertama kali diberikan saat anak umur 9 bulan. Campak-2 diberikan pada program BIAS SD kelas 1, umur 6 tahun</td>
+						</tr>
+						<td> <font color=#063b48>vk5</td><td> <font color=#063b48>vaksin hepatitis B</td><td><font color=#063b48>untuk mencegah terkenanya penyakit hepatitis B Hepatitis B diberikan tiga kali. Yang pertama dalam waktu 12 jam setelah lahir. Imunisasi ini dilanjutkan saat bayi berumur 1 bulan, kemudian diberikan l</td>
+						</tr>
+						
+						</table>
+						
+						</tr></td>
+				 </table>
+	
+		</tr></td>
+		</table>
+		
+				
+				
+				
+				</body>
+				</html>
