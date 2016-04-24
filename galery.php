e53bfa037328fdad51557f19cc9277ac38c7cfc6
Dennishariadi <Dennishariadi@gmail.com>
2016-04-24 15:54:19 +0700

    galery.php
    untuk menyimpan foto tentang posyandu sukalaya

---
+++ galery.php
@@ -0,0 +1,101 @@
+<html>
+	<head>
+		<title>POSYANDU BINA BAKTI</title>
+
+	</head>
+<body Background="img/1000.jpg">
+<br>
+<table align="center"  width="907"border=1 bordercolor=#84faea >
+<tr>
+
+			<th colspan=4 ><img src="img/PPIEU.jpg" width="907" height="208"></th>
+			</tr>
+			
+			</table>
+			<table align="center"  width="917"border=1 bordercolor=#84faea >
+			<tr>
+			<td width="137">
+			<a href="homeposyandu.php" title="halaman home">
+			<b><center><font size=2 font face="comic sans Ms" font color=#063b48>
+			Home</a> </b></center></td>
+	
+			
+			<TD width="146"><a href="inputdata.php" title="Silahkan Klik untuk input data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Input Data </center></td>
+			
+			<TD width="146"><a href="datakader.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Data Kader</center></td>
+			
+			<TD width="146"><a href="datahadir.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="Capture it" font color=#063b48>
+			Data hadir</center></td>
+			<TD width="170"><a href="databali.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Data Balita dan Imunisasi</b></center></td>
+			<TD width="170"><a href="upload.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Upload</b></center></td>
+			<TD width="170"><a href="galery.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Gallery</b></center></td>
+			<TD width="170"><a href="chat.php" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			Chat</b></center></td>
+			<TD width="170"><a href="index.html" title="Silahkan Klik untuk melihat data">
+			<center><font size=2 font face="comic sans ms" font color=#063b48><b><center>
+			LOGOUT</b></center></a></td>
+			</TABLE>
+		
+			
+			
+			<table align="center"  width="920"  >
+			
+			<tr><th Background="img/kl.gif" width="920" height="23"  ></th></tr>
+			</table>
+			
+			
+			<table align="center"  width="918" >
+			<tr><td><font size=4 font face="Harlow Solid Italic" font color=#063b48><center><marquee>
+			Pelayanan Bina Bakti Sukalaya Barat</marquee></center></td></tr>
+			</table>
+			
+			<table align="center" width="914"  Background="img/ba.jpg" border=1 bordercolor=#84faea>
+			<tr><td>
+<table align="center" width="914"  Background="img/ba.jpg" >
+			<tr><td>
+<align="left" ><font size=4 font face="Freehand521 BT" font color=#063b48>Posyandu Bina Bakti</B> <font size=3 font face="Berlin sanS FB" font color=#063b48>merupakan posyandu yang mencakup lingkungan di Sukalaya Barat RT 04 RW 08,pelayanan terpadu ini memiliki anggota anggota 5 orang, sering di sebut dengan kader posyandu, yang merupakan perwakilan dari tiap RT 
+dimana di adakan setiap bulan sekali, yaitu kegiatan imunisasi balita dan pemberian vaksin. dan kegitan penyuluhan lainnya.</font></tr></td>
+			
+</tr></td>
+<tr><td><hr width=100% ></hr></tr></td>
+
+<tr><td><font size=4 font face="Freehand521 BT" font color=#063b48>
+			Daftar Galery	 </tr></td>
+<tr><td>
+<table width="100%">
+		<?php
+			include "librari/koneksi.php";
+			$s = "select * from galery";
+			$q = mysql_query($s);
+			while($hasil = mysql_fetch_array($q)){
+		?>
+				<tr>
+					<td align="center"><img src="img/<?php echo $hasil['gambar']?>" width="400px" height="200px"></td>
+			</td>
+				</tr>
+		<?php
+			}
+		?>
+</table>
+	
+	
+		</tr></td>
+		</table>
+		
+				
+				
+				
+				</body>
+				</html>
